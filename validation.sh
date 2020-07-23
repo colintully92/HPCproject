de3e759189ac801de77608c52c719fe64db026be
colintully92 <colin.tully@env.ethz.ch>
2020-07-23 12:00:22 +0200

    Validation script now runs proper executables

--- validation.sh
+++ validation.sh
@@ -6,13 +6,13 @@
 echo "running stencil2d-mpi.F90 ..."
 #cd ../HPC4WC/day3 && \
   #srun stencil2d.py --nx=128 --ny=128 --nz=64 --num_iter=${num_iter} && \
-srun -n 12 ./stencil2d-mpi.x+orig --nx 128 --ny 128 --nz 64 --num_iter 1024
+srun -n 12 ./stencil2d-mpi.x --nx 128 --ny 128 --nz 64 --num_iter 1024
   #cd ../../Project || exit
 
 # run the programm to validate
 echo "running stencil2d-mpiomp.F90 ..."
 #srun stencil2d-cupy.py --nx=128 --ny=128 --nz=64 --num_iter=${num_iter}
-srun -n 12 ./stencil2d-mpiomp.x+orig --nx 128 --ny 128 --nz 64 --num_iter 1024
+srun -n 12 ./stencil2d-mpiomp.x --nx 128 --ny 128 --nz 64 --num_iter 1024
 
 # compare output againts control data
 echo "running compare_fields.py ..."
