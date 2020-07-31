848ca92aba1fa79fbea9461abdcca4df4e8f32ed
colintully92 <colin.tully@env.ethz.ch>
2020-07-31 10:47:33 +0200

    Whoops, delted run_hybrid by accident

---
+++ run_hybrid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#
+#SBATCH --time=00:30:00
+#SBATCH --ntasks=20
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --ntasks-per-core=2
+
+export OMP_NUM_THREADS=1
+
+srun ./stencil2d-mpiomp.x --nx 128 --ny 128 --nz 64 --num_iter 1024
+
+#NOTES
+#MPI ranks = ntasks
+#divide mpi ranks over nodes = ntasks-per-node
